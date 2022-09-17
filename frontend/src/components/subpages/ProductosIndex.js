211e8535c03b4bffdeb4d61430431d91f91e4de9
sergiorivas12 <sergio.rivas12@gmail.com>
2022-09-17 16:40:46 -0300

    fix desa

--- frontend/src/components/subpages/ProductosIndex.js
+++ frontend/src/components/subpages/ProductosIndex.js
@@ -2,14 +2,41 @@ import React from 'react';
 import Producto from '../layout/Producto';
 
 const ProductosIndex = (props) => {
+    let productos = [
+        {
+            "img": "img/almohada1.jpeg",
+            "titulo": "Almohada Viscoelastica King Bed & Co",
+            "precio": "$6,350.00",
+        },
+        {
+            "img": "img/almohada2.jpeg",
+            "titulo": "Almohada Sublime Cannon",
+            "precio": "$4,699.00",
+        },
+        {
+            "img": "img/almohada3.jpeg",
+            "titulo": "Almohada Viscoelástica Kids Bed & Co.",
+            "precio": "$3,099.00",
+        },
+        {
+            "img": "img/almohada4.jpeg",
+            "titulo": "Almohada Viscoelástica Relax Bed & Co.",
+            "precio": "$3,799.00",
+        },
+        {
+            "img": "img/almohada5.jpeg",
+            "titulo": "Almohada viscoelastica Cervical Bed&Co",
+            "precio": "$4,950.00",
+        },
+        {
+            "img": "img/almohada6.jpeg",
+            "titulo": "Almohada viscoelastica Clasica Bed&Co",
+            "precio": "$4,950.00",
+        },
+    ];
     return (
             <div className="productos">
-                <Producto img='img/almohada1.jpeg' titulo='Almohada Viscoelastica King Bed & Co' precio='$6,350.00'/>
-                <Producto img='img/almohada2.jpeg' titulo='Almohada Sublime Cannon' precio='$4,699.00'/>
-                <Producto img='img/almohada3.jpeg' titulo='Almohada Viscoelástica Kids Bed & Co.' precio='$3,099.00'/>
-                <Producto img='img/almohada4.jpeg' titulo='Almohada Viscoelástica Relax Bed & Co.' precio='$3,799.00'/>
-                <Producto img='img/almohada5.jpeg' titulo='Almohada viscoelastica Cervical Bed&Co' precio='$4,950.00'/>
-                <Producto img='img/almohada6.jpeg' titulo='Almohada viscoelastica Clasica Bed&Co' precio='$4,950.00'/>
+                {productos.map(producto => <Producto img={producto.img} titulo={producto.titulo} precio={producto.precio} />)}
             </div>
             );
 }
--- frontend/src/components/subpages/ProductosNuevos.js
+++ frontend/src/components/subpages/ProductosNuevos.js
@@ -2,16 +2,51 @@ import React from 'react';
 import Producto from '../layout/Producto';
 
 const ProductosNuevos = (props) => {
+    let productos = [
+        {
+            "img": "img/nueva1.jpg",
+            "titulo": "Combo x6 Uni. Silla Andina Chenille Chocolate",
+            "precio": "$50,999.00",
+        },
+        {
+            "img": "img/nueva2.jpg",
+            "titulo": "Combo x6 Uni. Silla Andina Chenille Cielo",
+            "precio": "$50,999.00",
+        },
+        {
+            "img": "img/nueva3.jpeg",
+            "titulo": "Sofá Esquinero de 2.10 Mts Hammer Gris Oscuro",
+            "precio": "$69,999.00",
+        },
+        {
+            "img": "img/nueva4.jpeg",
+            "titulo": "Sillón Matero de 1 Cuerpo Chenille Gris Oscuro",
+            "precio": "$9,999.00",
+        },
+        {
+            "img": "img/nueva5.jpeg",
+            "titulo": "Sillón Matero de 1 Cuerpo Chenille Gris Claro",
+            "precio": "$9,999.00",
+        },
+        {
+            "img": "img/nueva6.jpeg",
+            "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Tostado",
+            "precio": "$13,999.00",
+        },
+        {
+            "img": "img/nueva7.jpeg",
+            "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Gris Oscuro",
+            "precio": "$13,999.00",
+        },
+        {
+            "img": "img/nueva8.jpeg",
+            "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Gris Claro",
+            "precio": "$13,999.00",
+        },
+    ];
     return (
             <div className="productosNuevos">
-                <Producto class='productoNuevo' img='img/nueva1.jpg' titulo='Combo x6 Uni. Silla Andina Chenille Chocolate' precio='$50,999.00'/>
-                <Producto class='productoNuevo' img='img/nueva2.jpg' titulo='Combo x6 Uni. Silla Andina Chenille Cielo' precio='$50,999.00'/>
-                <Producto class='productoNuevo' img='img/nueva3.jpeg' titulo='Sofá Esquinero de 2.10 Mts Hammer Gris Oscuro' precio='$69,999.00'/>
-                <Producto class='productoNuevo' img='img/nueva4.jpeg' titulo='Sillón Matero de 1 Cuerpo Chenille Gris oscuro' precio='$9,999.00'/>
-                <Producto class='productoNuevo' img='img/nueva5.jpeg' titulo='Sillón Matero de 1 Cuerpo Chenille Gris Claro' precio='$9,999.00'/>
-                <Producto class='productoNuevo' img='img/nueva6.jpeg' titulo='Sillón Individual con apoya Brazos 1 cuerpo Tostado' precio='$13,999.00'/>
-                <Producto class='productoNuevo' img='img/nueva7.jpeg' titulo='Sillón Individual con apoya Brazos 1 cuerpo Gris Oscuro' precio='$13,999.00'/>
-                <Producto class='productoNuevo' img='img/nueva8.jpeg' titulo='Sillón Individual con apoya Brazos 1 cuerpo Gris Claro' precio='$13,999.00'/>
+                {productos.map(producto => <Producto class='productoNuevo' img={producto.img} titulo={producto.titulo} precio={producto.precio} />)}
             </div>
             );
 }
