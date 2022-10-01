03ae35f1e0247c2fd9ecf0e782545652c80cf1be
sergiorivas12 <sergio.rivas12@gmail.com>
2022-10-01 18:46:31 -0300

    fix desa

--- backend/routes/admin/login.js
+++ backend/routes/admin/login.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/login',{layout: 'admin/layout'});
+  var conocido = Boolean(req.session.user);
+  res.render('admin/login',{
+      title     : 'Sesiones ',
+      conocido  : conocido,
+      nombre    : req.session.user,
+      layout    : 'admin/layout'
+  });
+});
+
+router.post('/ingresar', function(req,res){
+    if(req.body.user){
+        req.session.user = req.body.user;
+    }
+    res.redirect('/');
+});
+
+router.get('/salir', function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
 });
 
 module.exports = router;
