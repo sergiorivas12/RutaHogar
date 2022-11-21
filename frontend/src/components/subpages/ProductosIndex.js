7c979ac264181f2a2412812baf9ccae9a0ed1413
sergiorivas12 <sergio.rivas12@gmail.com>
2022-11-21 17:02:53 -0300

    fix desa

--- frontend/src/components/subpages/ProductosIndex.js
+++ frontend/src/components/subpages/ProductosIndex.js
@@ -1,42 +1,60 @@
 import React from 'react';
+import axios from 'axios';
 import Producto from '../layout/Producto';
+import { useState, useEffect } from 'react';
 
 const ProductosIndex = (props) => {
-    let productos = [
-        {
-            "img": "img/almohada1.jpeg",
-            "titulo": "Almohada Viscoelastica King Bed & Co",
-            "precio": "$6,350.00",
-        },
-        {
-            "img": "img/almohada2.jpeg",
-            "titulo": "Almohada Sublime Cannon",
-            "precio": "$4,699.00",
-        },
-        {
-            "img": "img/almohada3.jpeg",
-            "titulo": "Almohada Viscoelástica Kids Bed & Co.",
-            "precio": "$3,099.00",
-        },
-        {
-            "img": "img/almohada4.jpeg",
-            "titulo": "Almohada Viscoelástica Relax Bed & Co.",
-            "precio": "$3,799.00",
-        },
-        {
-            "img": "img/almohada5.jpeg",
-            "titulo": "Almohada viscoelastica Cervical Bed&Co",
-            "precio": "$4,950.00",
-        },
-        {
-            "img": "img/almohada6.jpeg",
-            "titulo": "Almohada viscoelastica Clasica Bed&Co",
-            "precio": "$4,950.00",
-        },
-    ];
+    const [loading, setLoading] = useState(false);
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        const cargarProductos = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/productos');
+            setProductos(response.data);
+            setLoading(false);
+        };
+        cargarProductos();
+    }, []);
+    /*let productos = [
+     {
+     "img": "img/almohada1.jpeg",
+     "titulo": "Almohada Viscoelastica King Bed & Co",
+     "precio": "$6,350.00",
+     },
+     {
+     "img": "img/almohada2.jpeg",
+     "titulo": "Almohada Sublime Cannon",
+     "precio": "$4,699.00",
+     },
+     {
+     "img": "img/almohada3.jpeg",
+     "titulo": "Almohada Viscoelástica Kids Bed & Co.",
+     "precio": "$3,099.00",
+     },
+     {
+     "img": "img/almohada4.jpeg",
+     "titulo": "Almohada Viscoelástica Relax Bed & Co.",
+     "precio": "$3,799.00",
+     },
+     {
+     "img": "img/almohada5.jpeg",
+     "titulo": "Almohada viscoelastica Cervical Bed&Co",
+     "precio": "$4,950.00",
+     },
+     {
+     "img": "img/almohada6.jpeg",
+     "titulo": "Almohada viscoelastica Clasica Bed&Co",
+     "precio": "$4,950.00",
+     },
+     ];*/
     return (
             <div className="productosIndex">
-                {productos.map((producto, index) => <Producto key={index} img={producto.img} titulo={producto.titulo} precio={producto.precio} />)}
+                {loading ? (
+                                    <p>Cargando...</p>
+                                    ) : (
+                            productos.map((producto, index) => <Producto key={index} img={producto.imagen} titulo={producto.nombre} precio={producto.precio} />)
+                            )}
             </div>
             );
 }
--- frontend/src/components/subpages/ProductosNuevos.js
+++ frontend/src/components/subpages/ProductosNuevos.js
@@ -1,57 +1,74 @@
 import React from 'react';
+import axios from 'axios';
 import Producto from '../layout/Producto';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const ProductosNuevos = (props) => {
     const [cantidad, setCantidad] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [productos, setProductos] = useState([]);
 
-    let productos = [
-        {
-            "img": "img/nueva1.jpg",
-            "titulo": "Combo x6 Uni. Silla Andina Chenille Chocolate",
-            "precio": "$50,999.00",
-        },
-        {
-            "img": "img/nueva2.jpg",
-            "titulo": "Combo x6 Uni. Silla Andina Chenille Cielo",
-            "precio": "$50,999.00",
-        },
-        {
-            "img": "img/nueva3.jpeg",
-            "titulo": "Sofá Esquinero de 2.10 Mts Hammer Gris Oscuro",
-            "precio": "$69,999.00",
-        },
-        {
-            "img": "img/nueva4.jpeg",
-            "titulo": "Sillón Matero de 1 Cuerpo Chenille Gris Oscuro",
-            "precio": "$9,999.00",
-        },
-        {
-            "img": "img/nueva5.jpeg",
-            "titulo": "Sillón Matero de 1 Cuerpo Chenille Gris Claro",
-            "precio": "$9,999.00",
-        },
-        {
-            "img": "img/nueva6.jpeg",
-            "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Tostado",
-            "precio": "$13,999.00",
-        },
-        {
-            "img": "img/nueva7.jpeg",
-            "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Gris Oscuro",
-            "precio": "$13,999.00",
-        },
-        {
-            "img": "img/nueva8.jpeg",
-            "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Gris Claro",
-            "precio": "$13,999.00",
-        },
-    ];
+    useEffect(() => {
+        const cargarProductos = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/productos');
+            setProductos(response.data);
+            setLoading(false);
+        };
+        cargarProductos();
+    }, []);
+
+    /*let productos = [
+     {
+     "img": "img/nueva1.jpg",
+     "titulo": "Combo x6 Uni. Silla Andina Chenille Chocolate",
+     "precio": "$50,999.00",
+     },
+     {
+     "img": "img/nueva2.jpg",
+     "titulo": "Combo x6 Uni. Silla Andina Chenille Cielo",
+     "precio": "$50,999.00",
+     },
+     {
+     "img": "img/nueva3.jpeg",
+     "titulo": "Sofá Esquinero de 2.10 Mts Hammer Gris Oscuro",
+     "precio": "$69,999.00",
+     },
+     {
+     "img": "img/nueva4.jpeg",
+     "titulo": "Sillón Matero de 1 Cuerpo Chenille Gris Oscuro",
+     "precio": "$9,999.00",
+     },
+     {
+     "img": "img/nueva5.jpeg",
+     "titulo": "Sillón Matero de 1 Cuerpo Chenille Gris Claro",
+     "precio": "$9,999.00",
+     },
+     {
+     "img": "img/nueva6.jpeg",
+     "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Tostado",
+     "precio": "$13,999.00",
+     },
+     {
+     "img": "img/nueva7.jpeg",
+     "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Gris Oscuro",
+     "precio": "$13,999.00",
+     },
+     {
+     "img": "img/nueva8.jpeg",
+     "titulo": "Sillón Individual con apoya Brazos 1 cuerpo Gris Claro",
+     "precio": "$13,999.00",
+     },
+     ];*/
 
     //setCantidad(1);
     return (
             <div className="productosNuevos">
-                {productos.map((producto, index) => <Producto key={index} class='productoNuevo' img={producto.img} titulo={producto.titulo} precio={producto.precio} />)}
+                {loading ? (
+                                    <p>Cargando...</p>
+                                    ) : (
+                            productos.map((producto, index) => <Producto key={index} class='productoNuevo' img={producto.imagen} titulo={producto.nombre} precio={producto.precio} />)
+                            )}
             </div>
             );
 }
