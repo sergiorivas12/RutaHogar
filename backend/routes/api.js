09d8ff2d33b5670b3ffdb753326ca37f76e0e81d
sergiorivas12 <sergio.rivas12@gmail.com>
2022-11-27 15:29:26 -0300

    fix desa

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var productosModel = require('../models/productosModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/productos', async function (req, res, next) {
@@ -11,7 +12,7 @@ router.get('/productos', async function (req, res, next) {
             const imagen = cloudinary.url(producto.img_id, {});
             return {
                 ...producto,
-                caracteristicas : producto.descripcion.split(","),
+                caracteristicas: producto.descripcion.split(","),
                 imagen
             }
         } else {
@@ -23,6 +24,32 @@ router.get('/productos', async function (req, res, next) {
     });
     console.log(productos);
     res.json(productos);
+});
+
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'sergio.rivas12@gmail.com',
+        subject: 'Contacto Web',
+        html: req.body.nombre + ' se contacto a traves de la web y quiere mas informacion a este correo: ' + req.body.email
+                + '</br> Ademas, hizo el siguiente comentario: ' + req.body.mensaje
+                + '</br> su tel es: ' + req.body.telefono
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
+        }
+    });
+
+    await transport.sendMail(mail);
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje Enviado',
+    });
 });
 
 module.exports = router;
--- frontend/src/components/subpages/FormularioContacto.js
+++ frontend/src/components/subpages/FormularioContacto.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import Espacio from '../layout/Espacio';
 
 const textAreaStyle = {height: 260};
 const FormularioContacto = (props) => {
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: '',
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setFormData(oldData => ({
+                ...oldData,
+                [name]: value
+            }));
+    };
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm);
+        }
+    };
+
     return (
             <div className="row">
                 <div className="col-md-4">
@@ -15,18 +47,18 @@ const FormularioContacto = (props) => {
                     </div>
                 </div>
                 <div className="col-md-8">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="row">
                             <div className="col-md-6">
                                 <div className="form-group">
                                     <label htmlFor="nombre">Nombre (*)</label>
-                                    <input className="form-control" id="nombre" placeholder="Nombre" />
+                                    <input className="form-control" id="nombre" name="nombre" placeholder="Nombre" value={formData.nombre} onChange={handleChange}/>
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-group">
                                     <label htmlFor="email">Email (*)</label>
-                                    <input type="email" className="form-control" id="email" placeholder="Ingrese Email" />
+                                    <input type="email" className="form-control" id="email" name="email" placeholder="Ingrese Email" value={formData.email} onChange={handleChange}/>
                                 </div>
                             </div>
 
@@ -34,8 +66,8 @@ const FormularioContacto = (props) => {
                         <div className="row">
                             <div className="col-md-12">
                                 <div className="form-group">
-                                    <label htmlFor="asunto">Asunto</label>
-                                    <input className="form-control" id="asunto" placeholder="Asunto" />
+                                    <label htmlFor="asunto">Telefono</label>
+                                    <input className="form-control" id="telefono" name="telefono" placeholder="Telefono" value={formData.telefono} onChange={handleChange}/>
                                 </div>
                             </div>
                         </div>
@@ -43,10 +75,12 @@ const FormularioContacto = (props) => {
                             <div className="col-md-12">
                                 <div className="form-group">
                                     <label htmlFor="asunto">Mensaje</label>
-                                    <textarea style={textAreaStyle} className="form-control" id="mensaje" placeholder="mensaje"></textarea>
+                                    <textarea style={textAreaStyle} className="form-control" id="mensaje" name="mensaje" placeholder="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
                                 </div>
                             </div>
                         </div>
+                        {sending ? <p>Enviando...</p> : null}
+                        {msg ? <p>{msg}</p> : null}
                         <Espacio height='10px'/>
                         <div className="row">
                             <div className="col-md-12">
